b6eb6fc1f5699499913ad88570aabfc3897a0555
Brenden-Trieu <brenden.trieu@gmail.com>
2025-05-05 17:28:49 -0700

    inital step 1

---
+++ dynamicModal.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+  document.querySelector("#hbspt-form-1746476489000-1525090073").innerHTML = `
+    
+            <header>
+                <h1>Hello Conversion!</h1>
+            </header>
+            <p>Click on the button below to contact us</p>
+            <div class="header__links--contact">
+                <a class="button" href="#">Click here</a>
+            </div>
+`;
+
+  document.querySelectorAll(".contact-form__form")[0].style.minWidth =
+    "max-content";
+});
